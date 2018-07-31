ce039e8979355bdcf21d5935b6be59ae0855e542
Naoghuman <peter.rogge@yahoo.de>
2018-07-31 19:47:25 +0200

    Rename package color to shape.color.

--- src/test/java/com/github/naoghuman/yin/yang/shape/color/ColorComboBoxDemo.java
+++ src/test/java/com/github/naoghuman/yin/yang/shape/color/ColorComboBoxDemo.java
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package com.github.naoghuman.yin.yang.color;
+package com.github.naoghuman.yin.yang.shape.color;
 
 import javafx.application.Application;
 import javafx.collections.FXCollections;
